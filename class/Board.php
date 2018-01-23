00c4baae24cc055494de7d0c9987410f83984cea
Thomas Deroua <thomasderoua@hotmail.com>
2018-01-23 15:33:40 +0100

    push class

---
+++ class/Board.php
@@ -0,0 +1,11 @@
+<?php
+    Class Board 
+    {
+
+        // STOCKER LES 24 EMPLACEMENTS POUR LES 24 FLECHES
+
+        public function __construct() {
+
+        }
+    }
+?>
---
+++ class/Player.php
@@ -0,0 +1,14 @@
+<?php
+    Class Player 
+    {
+        protected $name =  null;
+        protected $pions = []; // liste de pions
+        protected $color = null; // 1 ou 2
+        protected $score = 0;
+
+        public function __construct($name, $color) {
+            $this->name = $name;
+            $this->color = $color;
+        }
+    }
+?>
